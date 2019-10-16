0c00768323e16bcd9d6488d34a58f5ccc998895a
Chris Russell <48478211+chrisrivendi@users.noreply.github.com>
2019-10-16 15:16:36 +0100

    Parallelisation of process

--- Transport.Cli/Program.cs
+++ Transport.Cli/Program.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Xml.Serialization;
 using Transport.BusStops;
 using Transport.TransXChange;
+using System.Threading.Tasks.Dataflow;
+
 
 namespace Transport.Cli
 {
@@ -14,19 +17,39 @@ namespace Transport.Cli
         {
             var path = args[0];
 
+            var degreeParallelism = new ExecutionDataflowBlockOptions()
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            };
+
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+
+            var fileDiscoveryBlock = new TransformManyBlock<string, string>(dirName =>
+                {
+                    return Directory.EnumerateFiles(path).Where(x => x.EndsWith(".xml"));
+                });
+
+            var processingBlock = new ActionBlock<string>(file =>
+            {
+                Console.WriteLine(file);
+                var txc = new TXCParser(file);
+            }, degreeParallelism);
+
             if (Directory.Exists(path))
             {
-                foreach (var file in Directory.EnumerateFiles(path).Where(x => x.EndsWith(".xml")))
-                {
-                    //Console.WriteLine(file);
-                    var txc = new TXCParser(file);
-                }
+                fileDiscoveryBlock.LinkTo(processingBlock, linkOptions);
+
+                fileDiscoveryBlock.Post(path);
+
+                fileDiscoveryBlock.Complete();
+                processingBlock.Completion.Wait();
             }
 
             else if (File.Exists(path))
             {
-                //Console.WriteLine(path);
-                var txc = new TXCParser(path);
+                processingBlock.Post(path);
+                processingBlock.Complete();
+                processingBlock.Completion.Wait();
             }
         }
     }
