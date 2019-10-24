41b94d75713a200840d9a2848a9f5de8741d1c16
Chris Russell <48478211+chrisrivendi@users.noreply.github.com>
2019-10-24 13:18:27 +0100

    Candidate query for nearest point on line

--- PostGIS/sql/nearest_road_to_stop.sql
+++ PostGIS/sql/nearest_road_to_stop.sql
@@ -4,3 +4,15 @@ FROM stops p
     LEFT JOIN roadlinks r ON ST_DWithin(p.geom, r.geom, 100)
 --WHERE p.atcocode LIKE '4500%'
 ORDER BY stop_id, ST_Distance(p.geom, r.geom), road_id
+
+CREATE TABLE stopcandidates AS 
+ SELECT t.stop_id, t.road_id, ST_ClosestPoint(t.stop_geom, t.road_geom) AS geom
+ FROM (
+		SELECT DISTINCT ON (p.gid)
+		p.gid AS stop_id, r.gid as road_id, ST_Distance(p.geom, r.geom) AS distance, p.geom AS stop_geom, r.geom AS road_geom
+		FROM stops p
+			LEFT JOIN roadlinks r ON ST_DWithin(p.geom, r.geom, 30)
+		WHERE p.atcocode LIKE '4500%'
+		ORDER BY stop_id, distance, road_id
+	) AS t,
+	roadlinks AS rl, stops AS s;
